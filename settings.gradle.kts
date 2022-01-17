cc8b9563cafa42eacc4d7263655c75b2bf14be56
Peter Trifanov <peter.trifanov@mail.ru>
2022-01-17 17:29:48 +0300

    chore: Initialize project

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "save-adapters"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+enableFeaturePreview("VERSION_CATALOGS")
