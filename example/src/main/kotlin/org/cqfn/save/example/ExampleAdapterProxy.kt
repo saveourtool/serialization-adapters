e78b28c5ed1a194a6dc4398d2346ded45e310f87
Peter Trifanov <peter.trifanov@mail.ru>
2022-01-20 15:48:28 +0300

    [skip ci] docs: Example
    * Update implementation details

--- example/src/main/kotlin/org/cqfn/save/example/ExampleAdapterProxy.kt
+++ example/src/main/kotlin/org/cqfn/save/example/ExampleAdapterProxy.kt
@@ -1,10 +1,31 @@
 package org.cqfn.save.example
 
-import org.cqfn.save.adapter.Adapter
+import io.github.detekt.sarif4k.ArtifactLocation
+import io.github.detekt.sarif4k.Location
+import io.github.detekt.sarif4k.Message
+import io.github.detekt.sarif4k.PhysicalLocation
+import io.github.detekt.sarif4k.Result
+import kotlinx.serialization.json.Json
 import org.cqfn.save.adapter.AdapterProxy
+import org.cqfn.save.adapter.SarifAdapter
 
 class ExampleAdapterProxy : AdapterProxy {
-    override fun adapter(): Adapter<out Any> {
-        return ExampleAdapter()
+    override fun adapter() = SarifAdapter<List<Example>>(
+        decoder = Json,
+    ) { exampleList ->
+        exampleList.map {
+            Result(
+                message = Message(text = it.message),
+                locations = listOf(
+                    Location(
+                        physicalLocation = PhysicalLocation(
+                            artifactLocation = ArtifactLocation(
+                                uri = it.name
+                            )
+                        )
+                    )
+                )
+            )
+        }
     }
 }
