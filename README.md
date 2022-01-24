68c5c064b063e1707bc27a6f841b665527367d04
Peter Trifanov <peter.trifanov@mail.ru>
2022-01-24 17:05:46 +0300

    build: Setup building of jar with dependencies, releases to Sonatype (#7)

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 ## Project's goal
 Convert any format into SARIF (or any other format too).
 
+## Building the project
+```bash
+./gradlew shadowJar
+```
+will produce a file `build/libs/save-adapters-<version>-all.jar`
+
+## Building the examples
+First, build main project, because examples depend on it:
+```bash
+./gradlew publishAllPublicationsToMavenLocalRepository
+```
+Then, navigate into examples' directory and run `gradle jar`.
+
 ## Adding a custom format
 - Implement a subclass of `AdapterProxy`. Preferable way is to use `SarifAdapter` factory method, but if you need more granular control of functionality, you can extend classes directly.
 - Create a Service Provider Interface file `META-INF/services/org.cqfn.save.adapter.AdapterProxy`
@@ -9,8 +22,8 @@ Convert any format into SARIF (or any other format too).
 ## Running the application
 Prerequisites: Java.
 
-Build a jar file for the custom format and place it on the classpath.
+Build a jar file for the custom format and place it on the classpath, along with `save-adapters-<version>-all.jar`.
 Then write serialized representation of your data into app's stdin:
 ```bash
-cat my-data.json | ./save-adapter
+cat my-data.json | java -cp *
 ```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     application
+    alias(libs.plugins.publish)
     `maven-publish`
+    signing
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.reckon)
 }
 
 dependencies {
@@ -9,6 +13,48 @@ dependencies {
     api(libs.sarif4k)
 }
 
+reckon {
+    scopeFromProp()
+    snapshotFromProp()
+}
+
 application {
     mainClass.set("org.cqfn.save.adapter.RunnerKt")
+}
+
+tasks {
+    shadowJar {
+        manifest {
+            attributes(
+                "Main-Class" to "org.cqfn.save.adapter.RunnerKt"
+            )
+        }
+    }
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["kotlin"])
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(System.getenv("GPG_SEC"), System.getenv("GPG_PASSWORD"))
+    sign(*extensions.getByType<PublishingExtension>().publications.toTypedArray())
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(findProperty("sonatypeUsername") as String?)
+            password.set(findProperty("sonatypePassword") as String?)
+        }
+    }
 }
