9ed184242e0a83ab736a1995df940a70f8d83a88
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-02-17 15:38:32 +0300

    Fix ### What's done: * Fix

--- examples/json-example/src/main/kotlin/org/cqfn/save/example/Example.kt
+++ examples/json-example/src/main/kotlin/org/cqfn/save/example/Example.kt
@@ -6,26 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Example(
     val filePath: String,
-    val message: String,
-    val refactoring: List<Statements>,
-)
-
-@Serializable
-data class Statements(
-    val statements: List<LineColumn>
-)
-
-@Serializable
-data class LineColumn(
-    @SerialName("start_line")
-    val startLine : Long,
-
-    @SerialName("start_column")
-    val startColumn : Long,
-
-    @SerialName("end_line")
-    val endLine : Long,
-
-    @SerialName("end_column")
-    val endColumn : Long,
+    val badSmellType: String,
+    val startLine: Long,
 )
